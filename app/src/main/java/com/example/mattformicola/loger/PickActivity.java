3a7514016e6cb166576152f0b2f93aeb19271d26
mform003 <mform003@plattsburgh.edu>
2018-11-15 18:51:44 -0500

    activitynmaps

--- app/src/main/java/com/example/mattformicola/loger/PickActivity.java
+++ app/src/main/java/com/example/mattformicola/loger/PickActivity.java
@@ -1,7 +1,9 @@
 package com.example.mattformicola.loger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PickActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class PickActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.WORKER:
+                finish();
+                startActivity(new Intent(this, MapsActivity.class));
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/example/mattformicola/loger/RegisterActivity.java
+++ app/src/main/java/com/example/mattformicola/loger/RegisterActivity.java
@@ -116,6 +116,12 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 finish();
                 startActivity(new Intent(this, MainActivity.class));
                 break;
+
+            case R.id.WORKER:
+                finish();
+                startActivity(new Intent(this, MapsActivity.class));
+                break;
+
         }
     }
 }
